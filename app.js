b8979cc258720847dbf4fb7ec73714e13e06d41a
Anand Soni <anandrajsonitheking@gmail.com>
2024-05-22 16:00:16 +0530

    parent only

--- app.js
+++ app.js
@@ -6,11 +6,11 @@
 
 const parent = React.createElement("div" ,
 {id: "parent"}, 
-  [  React.createElement("div" , { id: "child"}, 
+  [  React.createElement("div" , { id: "childa"}, 
        [ React.createElement("h1", {}, "Iam a H! tag"),
         React.createElement("h1", {}, "Iam a H! tag")
 ]),
-    React.createElement("div" , { id: "child"}, 
+    React.createElement("div" , { id: "childb"}, 
        [ React.createElement("h1", {}, "Iam a H! tag"),
         React.createElement("h1", {}, "Iam a H! tag")
 ]) 
@@ -18,14 +18,8 @@ const parent = React.createElement("div" ,
 )
 // parent is just an object, 
 // object is a react element
-const heading = React.createElement("h1" 
-,{id: "heading",
-    xyz: "react"
-},
- "Hellow world from react");
 
  const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
- root.render(heading)
  root.render(parent)
